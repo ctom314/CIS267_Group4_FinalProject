39b5ea1e7a0dfdf9d2ff3817a847a1c0097a1b3a
Isaac Shields <shieldsisaac0@gmail.com>
2024-11-12 15:40:18 -0500

    Added fixes to the placement script
    Items that are being placed will change colors based on if the item has a valid placement.
    
    Pressing Esc will cancel the placement.
    
    current program only works with square and circle colliders

--- Group4_FinalProject/Assets/Scripts/Placement.cs
+++ Group4_FinalProject/Assets/Scripts/Placement.cs
@@ -6,32 +6,81 @@ public class Placement : MonoBehaviour
 {
     public GameObject PlacementPrefab;
     private GameObject tempPrefab;
-    public bool moving = false;
+    private bool moving = false;
+    private Collider2D prefabCollider;
+    public LayerMask baseLayer;
+    private Color baseColor;
 
     void Update() 
     {
+        //spawn in the prefab, save the base color, set the color to a transparent green
         if(Input.GetKeyDown(KeyCode.Q) && !moving)
         {
-            Color baseColor = PlacementPrefab.GetComponent<SpriteRenderer>().color;
-            PlacementPrefab.GetComponent<SpriteRenderer>().color = new Color(23, 176, 23, 0.61f);
             Vector3 trueMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
             tempPrefab = Instantiate(PlacementPrefab, trueMousePos, Quaternion.identity);
+            baseColor = tempPrefab.GetComponent<SpriteRenderer>().color;
+            prefabCollider = tempPrefab.GetComponent<Collider2D>();
             moving = true;
         }
 
+        //follow the mouse
         if(moving)
         {
             Vector3 trueMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
             tempPrefab.transform.position = trueMousePos;
         }
 
-
-
-        if(moving && Input.GetMouseButtonDown(0))
+        //place the prefab and set the color back to it's normal color
+        if(moving && Input.GetMouseButtonDown(0) && validPlacement())
         {
-            Debug.Log("PLace!!");
             moving = false;
+            tempPrefab.GetComponent<SpriteRenderer>().color = baseColor;
+            tempPrefab.layer = 3;
+        }
 
+        //cancel the placement
+        if(moving && Input.GetKeyDown(KeyCode.Escape))
+        {
+            moving = false;
+            Destroy(tempPrefab);
+        }
+
+        //change color from green to red if it's in an invalid position
+        if(moving && !validPlacement())
+        {
+            //set color to red
+            tempPrefab.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 0.55f);
+        }
+        else if(moving && validPlacement())
+        {
+            //set color to green
+            tempPrefab.GetComponent<SpriteRenderer>().color = new Color(0, 143, 0, 0.5f);
         }
     }
+
+
+    private bool validPlacement()
+    {
+        //check if the item is colliding with another item
+        bool vp = false;
+        if(prefabCollider is BoxCollider2D bxc)
+        {
+            Collider2D tester = Physics2D.OverlapBox(tempPrefab.transform.position, new Vector2(bxc.transform.lossyScale.x, bxc.transform.lossyScale.y), 0, baseLayer);
+            if(tester == null)
+            {
+                vp = true;
+            }
+        }
+        else if(prefabCollider is CircleCollider2D cc)
+        {
+            Collider2D tester = Physics2D.OverlapCircle(tempPrefab.transform.position, cc.transform.lossyScale.x/2, baseLayer);
+            if(tester == null)
+            {
+                vp = true;
+            }
+        }
+
+        return vp;
+    }
+
 }
